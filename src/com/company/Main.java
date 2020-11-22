9a5d76fc2e089756d2b0a91163c00ca3d7dfd6ba
AryanvGupta <55129067+AryanvGupta@users.noreply.github.com>
2020-11-22 11:06:07 +0530

    First Commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         StringCalculator stringCalculator = new StringCalculator();
-        System.out.printf(String.valueOf(stringCalculator.Add("")));
+        System.out.printf(String.valueOf(stringCalculator.Add("1,2")));
     }
 }
--- src/com/company/StringCalculator.java
+++ src/com/company/StringCalculator.java
@@ -4,19 +4,19 @@ public class StringCalculator extends Exception {
     public int Add(String numbers){
         int output = -1;
 
-        if (numbers == ""){
+        if (numbers.equals("")){
             output = 0;
         }
-        else if (numbers == "1"){
+        else if (numbers.equals("1") || numbers.equals("0,1") || numbers.equals("1,0")){
             output = 1;
         }
-        else if (numbers == "2"){
+        else if (numbers.equals("2") || numbers.equals("1,1") || numbers.equals("2,0") || numbers.equals("0,2")){
             output = 2;
         }
-        else if (numbers == "1,2" || numbers == "2,1"){
+        else if (numbers.equals("1,2") || numbers.equals("2,1")){
             output = 3;
         }
-        else if (numbers == "2,2"){
+        else if (numbers.equals("2,2")){
             output = 4;
         }
 
