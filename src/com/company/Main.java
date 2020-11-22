1e3cc8bc6a089704985b6518917a28fb5ecefaed
AryanvGupta <55129067+AryanvGupta@users.noreply.github.com>
2020-11-22 11:29:58 +0530

    First Commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,14 @@ public class Main {
 
     public static void main(String[] args) {
         StringCalculator stringCalculator = new StringCalculator();
-        System.out.printf(String.valueOf(stringCalculator.Add("1,2")));
+
+        System.out.println(String.valueOf(stringCalculator.Add("")));
+        System.out.println(String.valueOf(stringCalculator.Add("1")));
+        System.out.println(String.valueOf(stringCalculator.Add("2")));
+        System.out.println(String.valueOf(stringCalculator.Add("1,2")));
+        System.out.println(String.valueOf(stringCalculator.Add("2,1")));
+        System.out.println(String.valueOf(stringCalculator.Add("2,2")));
+        System.out.println(String.valueOf(stringCalculator.Add("0,2")));
+        System.out.println(String.valueOf(stringCalculator.Add("1,0")));
     }
 }
