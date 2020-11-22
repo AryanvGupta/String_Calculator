584002e3b3f84b24db03ada75fc5be7174cc5459
AryanvGupta <55129067+AryanvGupta@users.noreply.github.com>
2020-11-22 11:30:41 +0530

    First Commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,13 +5,13 @@ public class Main {
     public static void main(String[] args) {
         StringCalculator stringCalculator = new StringCalculator();
 
-        System.out.println(String.valueOf(stringCalculator.Add("")));
-        System.out.println(String.valueOf(stringCalculator.Add("1")));
-        System.out.println(String.valueOf(stringCalculator.Add("2")));
-        System.out.println(String.valueOf(stringCalculator.Add("1,2")));
-        System.out.println(String.valueOf(stringCalculator.Add("2,1")));
-        System.out.println(String.valueOf(stringCalculator.Add("2,2")));
-        System.out.println(String.valueOf(stringCalculator.Add("0,2")));
-        System.out.println(String.valueOf(stringCalculator.Add("1,0")));
+        System.out.println(stringCalculator.Add(""));
+        System.out.println(stringCalculator.Add("1"));
+        System.out.println(stringCalculator.Add("2"));
+        System.out.println(stringCalculator.Add("1,2"));
+        System.out.println(stringCalculator.Add("2,1"));
+        System.out.println(stringCalculator.Add("2,2"));
+        System.out.println(stringCalculator.Add("0,2"));
+        System.out.println(stringCalculator.Add("1,0"));
     }
 }
